bfa84ea003adf38b8a7a445becc7fc42ad04234e
brunocfalcao <bruno.c.falcao@brunofalcao.me>
2019-06-24 17:01:31 +0200

    wip

--- src/LaraflashDALServiceProvider.php
+++ src/LaraflashDALServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace Laraflash\DAL;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Laraflash\DAL\Models\User;
 
 class LaraflashDALServiceProvider extends ServiceProvider
 {
@@ -12,11 +14,14 @@ class LaraflashDALServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
 
         $timestamp = date('Y_m_d_His', time());
+
         $this->publishes([
             __DIR__.'/../Database/Migrations/update_laraflash_schema_001.php.stub' => $this->app->databasePath()."/migrations/{$timestamp}_update_laraflash_schema_001.php",
         ], 'laraflash-update-schema-latest');
 
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        Auth::newGuard('laraflash-wave', User::class);
     }
 
     public function register()
